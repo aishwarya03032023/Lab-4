a95b9cc05b6d2a1c483f63f97b5049d484ae851c
aishwarya03032023 <145045908+aishwarya03032023@users.noreply.github.com>
2023-10-11 23:39:58 -0400

    Add files via upload

---
+++ Lab4_JavaSwing/src/main/java/com/mycompany/lab4_javaswing/Lab4_JavaSwing.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab4_javaswing;
+
+/**
+ *
+ * @author Admin
+ */
+public class Lab4_JavaSwing {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Lab4_JavaSwing/src/main/java/com/mycompany/lab4_javaswing/User.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab4_javaswing;
+
+/**
+ *
+ * @author Admin
+ */
+public class User {
+    private String name;
+    private String age;
+    private String type;
+    private String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
